9c8eb1d1106823c0fa61796bfd35d40928f3a50f
Guilherme De Marchi <70351793+Guilherme-De-Marchi@users.noreply.github.com>
2021-08-10 21:18:01 -0300

    Now it's 0.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Ugly Traveler',
-    version='1.0.0.0',
+    version='0.0.0',
     description='A Path Finder System',
     author='Guilherme de Marchi',
     author_email='guilhermedemarchi13@gmail.com',
